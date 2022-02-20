6e29299651a625ba9ed272242265da793d980f35
jumana727 <jumanajambu23@gmail.com>
2022-02-20 12:57:07 +0530

    commit

--- src/ViewController/Chatbot.jsx
+++ src/ViewController/Chatbot.jsx
@@ -5,8 +5,11 @@ import RightMsgUI from '../View/RightMsgUI';
 
 const Chatbot = ({ msgArray }) => {
   return (
-    <div>
-      <div>
+    <div className="container d-flex justify-content-center">
+      <div className="card mt-5">
+        <div className="d-flex flex-row justify-content-between p-3 adiv text-white">
+          <span className="pb-3">Live chat</span>
+        </div>
         {msgArray.map((msg, index) => {
           if (index % 2 === 0) {
             return <LeftMsgUI msg={msg} />;
