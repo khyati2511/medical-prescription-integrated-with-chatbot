b765439022be1708d0d4223e1e11e0a762ffe517
jumana727 <jumanajambu23@gmail.com>
2022-02-20 12:49:31 +0530

    commit

--- src/View/LeftMsgUI.jsx
+++ src/View/LeftMsgUI.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import profile from '../images/person.png';
 
 const LeftMsgUI = ({ msg }) => {
   return (
-    <div>
-      <div>
-        <p>{msg}</p>
+    <div className="d-flex flex-row p-3">
+      <img src={profile} width="30" height="30" alt="hsg" />
+      <div className="chat ml-2 p-3">
+        <span>{msg}</span>
       </div>
     </div>
   );
